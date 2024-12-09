44e897307bc5e6c50334107b70a50956f5e6b1f9
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-12-09 22:53:39 +0500

    Update PluginStatsController.php

--- app/Http/Controllers/PluginStatsController.php
+++ app/Http/Controllers/PluginStatsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 
 class PluginStatsController extends Controller {
@@ -16,30 +17,35 @@ class PluginStatsController extends Controller {
      * @return \Illuminate\Http\JsonResponse
      */
     public function download( Request $request, $slug ) {
-        $apiUrl = "https://api.wordpress.org/stats/plugin/1.0/downloads.php";
+        $cacheKey = "plugin_stats_{$slug}";
+        $apiUrl   = "https://api.wordpress.org/stats/plugin/1.0/downloads.php";
         try {
-            $response = Http::get( $apiUrl, [
-                'slug' => $slug,
-            ] );
-            if ( $response->successful() ) {
-                $data = $response->json();
-
-                return response()->json( [
-                    'success' => true,
-                    'data'    => $data,
+            $data = Cache::remember( $cacheKey, now()->addHours( 8 ), function () use ( $apiUrl, $slug ) {
+                $response = Http::get( $apiUrl, [
+                    'slug' => $slug,
                 ] );
-            } else {
-                return response()->json( [
-                    'success' => false,
-                    'message' => 'Failed to fetch plugin stats. Please try again later.',
-                ], 400 );
-            }
+
+              
+                if ( $response->successful() ) {
+                    return $response->json();
+                }
+                throw new \Exception( 'Failed to fetch plugin stats.' );
+            } );
+
+            return response()->json( [
+                'success' => true,
+                'data'    => $data,
+            ] );
         } catch ( RequestException $e ) {
             return response()->json( [
                 'success' => false,
                 'message' => 'An error occurred while fetching plugin stats: ' . $e->getMessage(),
             ], 500 );
+        } catch ( \Exception $e ) {
+            return response()->json( [
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500 );
         }
     }
 }
-
